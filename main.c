2bc81cded4ac283a7892dc7495ad3401df6d8337
Zantonlan <wilkeyvan@gmail.com>
2025-06-28 15:02:10 -0800

    Added some test stuff to window, made borderless fullscreen

--- main.c
+++ main.c
@@ -5,9 +5,40 @@ static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpara
 {
 	switch(msg)
 	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+			return 0;
+		}
+		case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hwnd, &ps);
+
+			int trapezoidpos[2] = {0, 0};
+			POINT trapezoid[4] = {
+				{trapezoidpos[1]+50, trapezoidpos[2]},
+				{trapezoidpos[1]+150, trapezoidpos[2]},
+				{trapezoidpos[1]+200, trapezoidpos[2]+100},
+				{trapezoidpos[1], trapezoidpos[2]+100},
+			};
+
+			Polygon(hdc, trapezoid, 4);
+
+			Rectangle(hdc, 0, 0, 10, 10);
+
+			TextOut(hdc, 10, 110, "Hey!", strlen("Hey!"));
+			EndPaint(hwnd, &ps);
+			return 0;
+		}
+		case WM_ERASEBKGND:
+		{
+			HDC hdc = (HDC)wparam;
+			RECT rc;
+			GetClientRect(hwnd, &rc);
+			FillRect(hdc, &rc, (HBRUSH)(COLOR_WINDOW + 1));
+			return 1;
+		}
 	}
 
 	return DefWindowProc(hwnd, msg, wparam, lparam);
@@ -24,19 +55,18 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdSh
 	wc.lpfnWndProc = WndProc;
 	wc.hInstance = hInst;
 	wc.lpszClassName = "Classy";
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 
-	if(!RegisterClassEx(&wc))
-		return 0;
+	if(!RegisterClassEx(&wc)) return 0;
 
 	hwnd = CreateWindowExA(0,
 		"Classy",
 		"Robert",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, 640, 480,
+		WS_POPUP, //WS_OVERLAPPEDWINDOW for reg. window
+		0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CXSCREEN), //CW_USEDEFAULT for default window position, 640x480 default window size
 		NULL, NULL, hInst, NULL);
 
-	if(!hwnd)
-		return 0;
+	if(!hwnd) return 0;
 
 	ShowWindow(hwnd, nCmdShow);
 	UpdateWindow(hwnd);
